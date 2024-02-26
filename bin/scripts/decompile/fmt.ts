72482d46f53f76249bec02841daad57c5ae2e912
noxabellus <noxabellus@gmail.com>
2024-02-26 16:59:33 -0600

    Fixed pretty printer in decompiler and removed shelljs dep

--- bin/scripts/decompile/fmt.ts
+++ bin/scripts/decompile/fmt.ts
@@ -1,14 +1,14 @@
 import * as prettier from 'prettier';
 
-import { __filename } from '../env.js';
-
 namespace fmt {
     export async function print(src: string | string[]): Promise<string> {
-        let options = await prettier.resolveConfig(__filename);
         if (Array.isArray(src)) src = src.join('\n');
 
         return prettier.format(src, {
-            ...options,
+            useTabs: false,
+            tabWidth: 4,
+            singleQuote: false,
+            trailingComma: 'all',
             parser: 'typescript',
         });
     }
